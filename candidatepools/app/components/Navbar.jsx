e03930b6744adb5ad62026565b9611dfa4ecb838
dechawat <69790549+dechawat0233@users.noreply.github.com>
2024-09-27 17:04:45 +0700

    edit

--- candidatepools/app/components/Navbar.jsx
+++ candidatepools/app/components/Navbar.jsx
@@ -198,20 +198,34 @@
 import React from "react";
 import Icon from "@mdi/react";
 import { mdiMonitor } from "@mdi/js";
+import { useTheme } from "../ThemeContext";
 
-function Navbar({
-  setFontSize,
-  setBgColor,
-  setBgColorNavbar,
-  setBgColorWhite,
-  fontSize,
-  bgColorNavbar,
-  bgColor,
-  bgColorWhite,
-}) {
+function Navbar({}) {
   // Accept setBgColor as a prop
   // console.log('bgColorNavbar',bgColorNavbar);
   // console.log('fontSize',fontSize);
+
+  const {
+    setFontSize,
+    setBgColor,
+    setBgColorNavbar,
+    setBgColorWhite,
+    setBgColorMain,
+    setBgColorMain2,
+    fontSize,
+    bgColorNavbar,
+    bgColor,
+    bgColorWhite,
+    bgColorMain,
+    setLineBlack,
+    lineBlack,
+    setTextBlue,
+    textBlue,
+    setRegisterColor,
+      registerColor,
+      setInputEditColor,
+      inputEditColor,
+  } = useTheme();
 
   return (
     // <div className='`${setBgColor}` px-5 py-2 flex items-center '>
@@ -258,25 +272,28 @@ function Navbar({
             setBgColor("bg-black text-white"); // Change background to black and text to white
             setBgColorNavbar("bg-black");
             setBgColorWhite("text-white");
+            setBgColorMain("bg-black");
+            setBgColorMain2("bg-black");
+            setLineBlack("border-white");
+            setTextBlue("text-white-500");
+            setRegisterColor("text-white");
+            setInputEditColor("bg-black-200");
           }}
           className="hover:cursor-pointer h-6 flex justify-center items-center rounded-lg bg-black text-white py-1 px-2 text-xs font-thin"
         >
           อักษรขาวพื้นดำ
         </div>
-        {/* <div 
-                    onClick={() => {
-                        setBgColor('bg-black text-[#e7e703]'); // Change background to black and text to yellow
-                        setBgColorNavbar('bg-black');
-
-                    }}
-                    className='hover:cursor-pointer h-6 flex justify-center items-center rounded-lg bg-black text-[#e7e703] py-1 px-2 text-xs font-thin'>
-                    อักษรเหลืองพื้นดำ
-                </div> */}
         <div
           onClick={() => {
             setBgColor("bg-black text-[#e7e703]"); // Change background to black and text to yellow
             setBgColorNavbar("bg-black"); // Change navbar background to black
             setBgColorWhite("text-[#e7e703]");
+            setBgColorMain("bg-black");
+            setBgColorMain2("bg-black");
+            setLineBlack("border-white");
+            setTextBlue("text-[#e7e703]-500");
+            setRegisterColor("text-[#e7e703]");
+            setInputEditColor("bg-black-200");
           }}
           className="hover:cursor-pointer h-6 flex justify-center items-center rounded-lg bg-black text-[#e7e703] py-1 px-2 text-xs font-thin"
         >
@@ -286,7 +303,14 @@ function Navbar({
           onClick={() => {
             setBgColor("bg-[#F4F6FA] text-black"); // Reset to default background and text color
             setBgColorNavbar("bg-[#F97201]");
+            // setBgColorNavbar("bg-white");
             setBgColorWhite("text-white");
+            setBgColorMain("#e6ffff");
+            setBgColorMain2("bg-white");
+            setLineBlack("border-black");
+            setTextBlue("text-blue-500");
+            setRegisterColor("text-[#F97201]");
+            setInputEditColor("bg-gray-200");
           }}
           className="hover:cursor-pointer h-6 flex justify-center items-center rounded-lg bg-white text-black py-1 px-2 text-xs font-thin"
         >
--- candidatepools/app/components/NavbarLogo.jsx
+++ candidatepools/app/components/NavbarLogo.jsx
@@ -4,10 +4,21 @@ import React from 'react'
 import Image from 'next/image'
 import { useState, useEffect } from 'react';
 import { useSession } from 'next-auth/react';
+import { useTheme } from '../ThemeContext';
 
-
-function NavbarLogo({title,dataUser,bgColorNavbar}) {
-
+function NavbarLogo({title,dataUser}) {
+    const {
+        setFontSize,
+        setBgColor,
+        setBgColorNavbar,
+        setBgColorWhite,
+        setBgColorMain,
+        fontSize,
+        bgColorNavbar,
+        bgColor,
+        bgColorWhite,
+        bgColorMain,
+      } = useTheme();
     // const { data: session } = useSession();
     // const [dataUser, setDataUser] = useState(null);
 
@@ -41,11 +52,11 @@ function NavbarLogo({title,dataUser,bgColorNavbar}) {
 
 
     return (
-        <div className={`${bgColorNavbar} relative gap-1 flex justify-between border-b-8  border-[#75C7C2] `}>
+        <div className={`${bgColorMain} relative gap-1 flex justify-between border-b-8  border-[#75C7C2] `}>
             {dataUser ? (
                 <div className=" flex items-end">
                     {/* <div className='h-full flex items-center bg-[#eeeeee] py-4 px-5 w-60 gap-5'> */}
-                    <div className={`h-full flex items-center ${bgColorNavbar} py-4 px-5 w-60 gap-5`}>
+                    <div className={`h-full flex items-center ${bgColorMain} py-4 px-5 w-60 gap-5`}>
                         <Image priority alt="icon" className='w-11 h-11 ' src={dataUser.profile || "/image/main/user.png"} height={1000} width={1000} />
                         <p className=' font-bold'>{dataUser.firstName} {dataUser.lastName}</p>
                     </div>
@@ -60,8 +71,9 @@ function NavbarLogo({title,dataUser,bgColorNavbar}) {
                 
             </div>
             <div className=" flex py-4 px-5">
-                <Image priority alt="icon" className='w-auto h-14' src="/image/main/eee.png" height={1000} width={1000} />
+                
                 <Image priority alt="icon" className='w-auto h-14' src="/image/main/iw.png" height={1000} width={1000} />
+                <Image priority alt="icon" className='w-auto h-14' src="/image/main/eee.png" height={1000} width={1000} />
                 <Image priority alt="icon" className='w-auto h-14' src="/image/main/sss.png" height={1000} width={1000} />
                 <div className="-z-10 bg-orange-200 w-10 h-14 rounded-tl-full absolute bottom-0 right-0"></div>
             </div>
