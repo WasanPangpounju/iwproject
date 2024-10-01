3483390de92273b261d999e6270764408184d239
pportler <pportler.sss@gmail.com>
2024-10-02 00:01:19 +0700

    add model education

---
+++ candidatepools/models/education.js
@@ -0,0 +1,44 @@
+import mongoose, { Schema } from "mongoose";
+
+const EducationSchema = new Schema(
+    {
+        email: {
+            type: String,
+            required: true
+        },
+        typePerson: {
+            type: String,
+        },
+        yearGraduation: {
+            type: [String],
+        },
+        faculty: {
+            type: [String],
+        },
+        branch: {
+            type: [String],
+        },
+        university: {
+            type: [String],
+        },
+        grade: {
+            type: [String],
+        },
+        nameDocument: {
+            type: [String],
+        },
+        sizeDocument: {
+            type: [String],
+        },
+        fileDocument: {
+            type: [String],
+        },
+
+    },
+    {
+        timestamps: true
+    }
+)
+
+const Educations = mongoose.models.Educations || mongoose.model("Educations", EducationSchema);
+export default Educations
