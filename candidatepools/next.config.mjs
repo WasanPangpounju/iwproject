70060adc15fe1dbc7d1f855db7557eef368006dc
WasanPangpounju <friendlydev.net@gmail.com>
2024-10-30 18:33:15 +0700

    test

--- candidatepools/next.config.mjs
+++ candidatepools/next.config.mjs
@@ -1,26 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'lh3.googleusercontent.com',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'profile.line-scdn.net',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'firebasestorage.googleapis.com',
-                pathname: '/**',
-            },
-        ],
-    },
+    images: { domains: ['lh3.googleusercontent.com', 'profile.line-scdn.net', 'firebasestorage.googleapis.com'] },
     assetPrefix: process.env.NODE_ENV === 'production' ? 'https://career.iwsif.org' : '',
     trailingSlash: true,
 };
-
 export default nextConfig;
