1c40adcda7b67a589adabc9f00b4186b3191480f
pportler <pportler.sss@gmail.com>
2024-10-30 13:30:45 +0700

    .

--- candidatepools/next.config.mjs
+++ candidatepools/next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['lh3.googleusercontent.com', 'profile.line-scdn.net', 'firebasestorage.googleapis.com'],
-    },
+    images: { domains: ['lh3.googleusercontent.com', 'profile.line-scdn.net', 'firebasestorage.googleapis.com'] },
     assetPrefix: process.env.NODE_ENV === 'production' ? 'https://career.iwsif.org' : '',
+    trailingSlash: true,
 };
-
 export default nextConfig;
