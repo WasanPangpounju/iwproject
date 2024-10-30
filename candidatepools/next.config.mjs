6d84125b77fa51b6d88dac382c783765330a2c98
WasanPangpounju <friendlydev.net@gmail.com>
2024-10-30 08:27:54 +0700

    Revert "update next config"
    This reverts commit 6e167f025011cf73e21bc9d6da5585c77fab2032.

--- candidatepools/next.config.mjs
+++ candidatepools/next.config.mjs
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['lh3.googleusercontent.com', 'profile.line-scdn.net', 'firebasestorage.googleapis.com']
-    },
-    reactStrictMode: true,       // Enables React strict mode
-    swcMinify: true,             // Enables SWC-based minification
-    trailingSlash: true,         // Ensures URLs end with a slash
-    // Uncomment basePath if app is hosted in a subdirectory:
-    // basePath: '/career.iwsif.org/iwproject/candidatepools',
+    images:{
+        domains:['lh3.googleusercontent.com','profile.line-scdn.net','firebasestorage.googleapis.com']
+    }
 };
 
 export default nextConfig;
