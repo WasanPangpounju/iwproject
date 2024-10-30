2f13533d67b5fd02d13c76d8b0738ddf049e81d0
WasanPangpounju <friendlydev.net@gmail.com>
2024-10-30 08:25:22 +0700

    Revert "Revert "update next config""
    This reverts commit bf2473b746744e5729e5603f73883bf6a011c34c.

---
+++ candidatepools/next.config - Copy.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images:{
+        domains:['lh3.googleusercontent.com','profile.line-scdn.net','firebasestorage.googleapis.com']
+    }
+};
+
+export default nextConfig;
