f90bbfb77efed98016d820bda0271df059061567
pportler <pportler.sss@gmail.com>
2025-02-08 00:34:38 +0700

    change layout in admin dashboad

--- candidatepools/app/supervisor/page.jsx
+++ candidatepools/app/supervisor/page.jsx
@@ -10,7 +10,7 @@ import { useRouter } from 'next/navigation'
 import { useTheme } from '../ThemeContext'
 import Icon from '@mdi/react'
 import { mdiAccountSchool, mdiAccountEdit, mdiFaceMan, mdiFaceWoman, mdiPlus, mdiContentSave, mdiCloseThick } from '@mdi/js'
-import BarChart from '@/app/supervisor/components/ChartDisabled'
+import PieChart from '@/app/supervisor/components/ChartDisabled'
 
 
 
@@ -60,6 +60,7 @@ function SupervisorPage() {
         if (session?.user?.id) {
             getUser(session.user.id);
             getDataAllUser();
+            getDataEducations();
         } else {
             router.replace("/agreement");
         }
@@ -119,6 +120,30 @@ function SupervisorPage() {
         }
     }
 
+    //get data educations
+    const [allEducations, setAllEducations] = useState([])
+    async function getDataEducations() {
+        try {
+            const res = await fetch(
+                `${process.env.NEXT_PUBLIC_BASE_API_URL}/api/educations`,
+                {
+                    method: "GET",
+                    cache: "no-store",
+                }
+            );
+
+            if (!res.ok) {
+                throw new Error("Error getting data from API");
+            }
+
+            const data = await res.json();
+            setAllEducations(data.educations || {});
+        } catch (err) {
+            console.error("Error fetching API", err);
+        }
+    }
+
+    //catalog_user
     const count_allUser = allUser?.length;
     const count_graduation = allUser?.filter((user) => user?.typePerson === "บัณฑิตพิการ")?.length;
     const count_students = allUser?.filter((user) => user?.typePerson === "นักศึกษาพิการ")?.length;
@@ -148,9 +173,43 @@ function SupervisorPage() {
         user?.typeDisabled?.some((disa) => disa === "พิการทางการออทิสติก")
     )?.length;
 
-    console.log(
-        allUser
-    )
+    //level
+    const count_level1 = allEducations?.filter((edu) => edu.level[0] === '1')?.length;
+    const count_level2 = allEducations?.filter((edu) => edu.level[0] === '2')?.length;
+    const count_level3 = allEducations?.filter((edu) => edu.level[0] === '3')?.length;
+    const count_level4 = allEducations?.filter((edu) => edu.level[0] === '4')?.length;
+
+    //student age
+    const currentYear = new Date().getFullYear();
+    console.log(currentYear)
+    const count_age11_20 = allUser?.filter((user) => {
+        const birthYear = Number(user?.yearBirthday)
+        const age = currentYear - birthYear;
+        return age >= 11 && age <= 20;
+    })?.length;
+    const count_age21_30 = allUser?.filter((user) => {
+        const birthYear = Number(user?.yearBirthday)
+        const age = currentYear - birthYear;
+        return age >= 21 && age <= 30;
+    })?.length;
+    const count_age31_40 = allUser?.filter((user) => {
+        const birthYear = Number(user?.yearBirthday)
+        const age = currentYear - birthYear;
+        return age >= 31 && age <= 40;
+    })?.length;
+    const count_age41_50 = allUser?.filter((user) => {
+        const birthYear = Number(user?.yearBirthday)
+        const age = currentYear - birthYear;
+        return age >= 41 && age <= 50;
+    })?.length;
+    const count_age50 = allUser?.filter((user) => {
+        const birthYear = Number(user?.yearBirthday)
+        const age = currentYear - birthYear;
+        return age > 50;
+    })?.length;
+
+    console.log(allUser)
+
     return (
         <div className={`${bgColorMain} ${bgColor} ${fontSize}`}>
             <NavbarLogo dataUser={dataUser} />
@@ -164,42 +223,97 @@ function SupervisorPage() {
                         ) : (
                             <>
                                 <p className='font-bold'>ข้อมูลผู้ใช้งานทั้งหมด</p>
-                                <div className={`${bgColorWhite} mt-5 flex justify-center gap-x-10 gap-y-7 flex-wrap`}>
-                                    <div className='bg-[#299d8f] rounded-sm py-2 w-64 flex flex-col justify-center items-center gap-1'>
-                                        <p>ทั้งหมด</p>
-                                        <p className='text-xl'>{count_allUser}</p>
-                                    </div>
-                                    <div className='bg-[#f4a261] rounded-sm px-10 py-2 w-64 gap-7 flex justify-center items-center'>
-                                        <Icon className='' path={mdiAccountSchool} size={2} />
-                                        <div className='flex flex-col justify-center items-center gap-1'>
-                                            <p>บัณฑิต</p>
-                                            <p className='text-xl'>{count_graduation}</p>
+                                <div className={`mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2`}>
+                                    <div className='flex flex-col justify-center gap-3 p-5 bg-gray-200 max-w-96'>
+                                        <div className='bg-[#ffa152] rounded-sm py-2  flex flex-col justify-center items-center gap-1'>
+                                            <p>ทั้งหมด</p>
+                                            <p className='text-xl'>{count_allUser}</p>
+                                        </div>
+                                        <div className='flex gap-3'>
+                                            <div className='bg-[#ffa152] py-2 rounded-sm flex flex-col items-center gap-1 w-[50%]'>
+                                                <Icon className='' path={mdiAccountSchool} size={1.2} />
+                                                <div className='flex flex-col justify-center items-center gap-1'>
+                                                    <p>บัณฑิต {count_graduation}</p>
+                                                </div>
+                                            </div>
+                                            <div className='bg-[#ffa152] py-2  rounded-sm flex flex-col justify-center items-center gap-1 w-[50%]'>
+                                                <Icon className='' path={mdiAccountEdit} size={1.2} />
+                                                <div className='flex flex-col justify-center items-center gap-1'>
+                                                    <p>นักศึกษา {count_students}</p>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
-                                    <div className='bg-[#e9c46a]  rounded-sm px-10 py-2 w-64 gap-7 flex justify-center items-center'>
-                                        <Icon className='' path={mdiAccountEdit} size={2} />
-                                        <div className='flex flex-col justify-center items-center gap-1'>
-                                            <p>นักศึกษา</p>
-                                            <p className='text-xl'>{count_students}</p>
-                                        </div>
-                                    </div>
-                                    <div className='bg-[#5494ca]  rounded-sm px-10 py-2 w-64 gap-7 flex justify-center items-center'>
-                                        <Icon className='' path={mdiFaceMan} size={2} />
-                                        <div className='flex flex-col justify-center items-center gap-1'>
-                                            <p>ชาย</p>
+                                    <div className='flex flex-col bg-gray-200 gap-3 p-5 max-w-96'>
+                                        <div className='bg-[#8aceff]  rounded-sm gap-1 py-2 flex flex-col justify-center items-center'>
+                                            <div className='flex items-center gap-2'>
+                                                <Icon className='' path={mdiFaceMan} size={1} />
+                                                <p>ชาย</p>
+                                            </div>
                                             <p className='text-xl'>{count_male}</p>
                                         </div>
-                                    </div>
-                                    <div className='bg-[#f8a9a9]  rounded-sm px-10 py-2 w-64 gap-7 flex justify-center items-center'>
-                                        <Icon className='' path={mdiFaceWoman} size={2} />
-                                        <div className='flex flex-col justify-center items-center gap-1'>
-                                            <p>หญิง</p>
+                                        <div className='bg-[#ffdddc]  rounded-sm gap-1 py-2 flex flex-col justify-center items-center'>
+
+                                            <div className='flex items-center gap-2'>
+                                                <Icon className='' path={mdiFaceWoman} size={1} />
+                                                <p>หญิง</p>
+                                            </div>
                                             <p className='text-xl'>{count_female}</p>
                                         </div>
                                     </div>
+                                    <div className='flex flex-col bg-gray-200 gap-3 p-5 max-w-96'>
+                                        <div className='bg-[#78dfc7]  rounded-sm gap-1 py-2 px-5 flex flex-col justify-center'>
+                                            <p>ระดับชั้น</p>
+                                            <div className='flex justify-between'>
+                                                <p>ปี 1</p>
+                                                <p>{`${count_level1 === 0 ? "-" : `${count_level1} คน`} `}</p>
+                                            </div>
+                                            <div className='flex justify-between'>
+                                                <p>ปี 2</p>
+                                                <p>{`${count_level2 === 0 ? "-" : `${count_level2} คน`} `}</p>
+                                            </div>
+                                            <div className='flex justify-between'>
+                                                <p>ปี 3</p>
+                                                <p>{`${count_level3 === 0 ? "-" : `${count_level3} คน`} `}</p>
+                                            </div>
+                                            <div className='flex justify-between'>
+                                                <p>ปี 4</p>
+                                                <p>{`${count_level4 === 0 ? "-" : `${count_level4} คน`} `}</p>
+                                            </div>
+                                            <div className='flex justify-between'>
+                                                <p>อื่นๆ</p>
+                                                <p>{`${count_graduation === 0 ? "-" : `${count_graduation} คน`} `}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className='flex flex-col bg-gray-200 gap-3 p-5 max-w-96'>
+                                        <div className='bg-[#a9eef9]  rounded-sm gap-1 py-2 px-5 flex flex-col justify-center'>
+                                            <p>ช่วงอายุ</p>
+                                            <div className='flex justify-between'>
+                                                <p>11-20 ปี</p>
+                                                <p>{`${count_age11_20 === 0 ? "-" : `${count_age11_20} คน`} `}</p>
+                                            </div>
+                                            <div className='flex justify-between'>
+                                                <p>21-30 ปี</p>
+                                                <p>{`${count_age21_30 === 0 ? "-" : `${count_age21_30} คน`} `}</p>
+                                            </div>
+                                            <div className='flex justify-between'>
+                                                <p>31-40 ปี</p>
+                                                <p>{`${count_age31_40 === 0 ? "-" : `${count_age31_40} คน`} `}</p>
+                                            </div>
+                                            <div className='flex justify-between'>
+                                                <p>41-50 ปี</p>
+                                                <p>{`${count_age41_50 === 0 ? "-" : `${count_age41_50} คน`} `}</p>
+                                            </div>
+                                            <div className='flex justify-between'>
+                                                <p>{`< 50`}</p>
+                                                <p>{`${count_age50 === 0 ? "-" : `${count_age50} คน`} `}</p>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div className='flex mt-10 overflow-scroll'>
-                                    <BarChart d1={count_d1} d2={count_d2} d3={count_d3} d4={count_d4} d5={count_d5} d6={count_d6} d7={count_d7} allStudents={count_graduation+count_students}/>
+                                <div className='flex mt-5 overflow-scroll'>
+                                    <PieChart d1={count_d1} d2={count_d2} d3={count_d3} d4={count_d4} d5={count_d5} d6={count_d6} d7={count_d7} allStudents={count_graduation + count_students} />
 
                                 </div>
                             </>
