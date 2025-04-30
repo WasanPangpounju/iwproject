8b480e2a15daa1352389cbe64b53be654fe2add2
Phitakpong.s <pportler.sss@gmail.com>
2025-05-01 00:53:12 +0700

    Update: credentials fixed maxAge

--- candidatepools/app/components/Loader.jsx
+++ candidatepools/app/components/Loader.jsx
@@ -5,14 +5,14 @@ import { BeatLoader } from 'react-spinners';
 function Loader() {
 
     return (
-        <div >
-            <div className="fixed top-0 z-10 opacity-50 bg-black w-screen h-screen"></div>
-            <div className="fixed top-0 z-30 flex justify-center items-center w-screen h-screen">
+        <>
+            <div className="fixed top-0 left-0 z-10 opacity-50 bg-black w-screen h-screen"></div>
+            <div className="fixed top-0 left-0 z-30 flex justify-center items-center w-screen h-screen">
                 <div>
                     <BeatLoader color="#F97201" />
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
