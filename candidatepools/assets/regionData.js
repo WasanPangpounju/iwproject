cacd4427243ec7f56fe58d4be693f0e72eab4bb7
Phitakpong.s <pportler.sss@gmail.com>
2026-02-04 23:26:34 +0700

    add: regionData for assets

---
+++ candidatepools/assets/regionData.js
@@ -0,0 +1,8 @@
+export const regionData = [
+  { id: 1, name: "เหนือ" },
+  { id: 2, name: "ตะวันออกเฉียงเหนือ" },
+  { id: 3, name: "กลาง" },
+  { id: 4, name: "ใต้" },
+  { id: 5, name: "ตะวันตก" },
+  { id: 6, name: "ตะวันออก" },
+];
