f424e38ca792bd9ab7123fdc062b6822940610aa
Phitakpong.s <pportler.sss@gmail.com>
2026-02-04 23:25:08 +0700

    update: text error component

--- candidatepools/app/components/TextError.jsx
+++ candidatepools/app/components/TextError.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-function TextError({ text }) {
-  return <p className="text-red-500 text-xs">* {text}</p>;
+function TextError({ text, className = "" }) {
+  return <p className={`text-red-500 text-xs ${className}`}>* {text}</p>;
 }
 
 export default TextError;
