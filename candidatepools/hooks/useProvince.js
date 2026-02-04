3db3126afbf8b0df038ebe5d21eb2724880f177d
Phitakpong.s <pportler.sss@gmail.com>
2026-02-04 23:25:55 +0700

    update: useProvince for get by regionId

--- candidatepools/hooks/useProvince.js
+++ candidatepools/hooks/useProvince.js
@@ -1,22 +1,21 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-export function useProvince() {
+export function useProvince(region_id) {
   const [dataProvince, setDataProvince] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await axios.get(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/provinces`);
+        let url = `${process.env.NEXT_PUBLIC_BASE_API_URL}/api/provinces?region_id=${region_id}`;
+        const res = await axios.get(url);
         setDataProvince(res.data);
-        
       } catch (err) {
         console.error("Error fetching province:", err);
       }
     };
-
     fetchData();
-  }, []);
+  }, [region_id]);
 
   return { dataProvince };
 }
